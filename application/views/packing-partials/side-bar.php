c91117ea39d8594439507d4be8633ad6737a9a6c
Alfian wahyu D <102296989+AlfianWD@users.noreply.github.com>
2022-11-26 15:55:20 +0700

    membuat tampilan produksi dan packing

---
+++ application/views/packing-partials/side-bar.php
@@ -0,0 +1,50 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url(); ?>admin/dashboard_packing">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">CV Faza Mega Berlian</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="<?= base_url(); ?>admin/dashboard_packing">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Packing
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fa fa-file"></i>
+            <span>Data</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Kelola Data:</h6>
+                <a class="collapse-item" href="<?= base_url(); ?>admin/pesanan_packing ">Pesanan</a>
+            </div>
+        </div>
+    </li>
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>QC</span>
+        </a>
+    </li>
